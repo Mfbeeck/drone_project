1b9234179bfcf5579b37637cb127dd540a766143
Mfbeeck <mfbeeck@gmail.com>
2015-07-24 11:30:04 -0400

    basic functionality working, will start attachments model now

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,12 +1,38 @@
 class GalleriesController < ApplicationController
+	include GalleriesHelper
 	def index
 		@galleries = Gallery.all
 	end
 	def show
 		@gallery = Gallery.find(params[:id])
+
+	end
+	def edit
+		@gallery = Gallery.find(params[:id])
 	end
 	def new
 		@gallery = Gallery.new
 	end
+	def create
+		@gallery = Gallery.new(gallery_params)
+		@gallery.save
+		redirect_to gallery_path(@gallery)
+		flash.notice = "The gallery \"#{@gallery.title}\" was successfully created."
+	end
+
+	def update
+		@gallery = Gallery.find(params[:id])
+		@gallery.update(gallery_params)
+		redirect_to gallery_path(@gallery)
+		flash.notice = "The gallery \"#{@gallery.title}\" was successfully updated."
+	end
+
+	def destroy
+		@gallery = Gallery.find(params[:id])
+		@gallery.destroy
+		redirect_to galleries_path
+		flash.notice = "The gallery \"#{@gallery.title}\" was successfully deleted."
+	end
+  	
 
 end
--- app/helpers/galleries_helper.rb
+++ app/helpers/galleries_helper.rb
@@ -1,2 +1,5 @@
 module GalleriesHelper
+	def gallery_params
+  		params.require(:gallery).permit(:title, :body, :created_at, :updated_at)
+	end
 end
