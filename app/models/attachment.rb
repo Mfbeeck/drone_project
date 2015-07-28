cf9eb1c51bef3adcdfe8089852bdc0a02ac2d2ba
Mfbeeck <mfbeeck@gmail.com>
2015-07-28 12:45:01 -0400

    fixed image sizing

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,5 +1,5 @@
 class Attachment < ActiveRecord::Base
   belongs_to :gallery
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: {large: "600x600>", medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
