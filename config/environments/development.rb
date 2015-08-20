f857140479c22f835b9732c376df3a07ba8ddd7e
Mfbeeck <mfbeeck@gmail.com>
2015-08-20 12:40:44 -0400

    yml stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => 'S3_BUCKET_NAME', :access_key_id => 'AWS_ACCESS_KEY_ID', :secret_access_key => 'AWS_SECRET_ACCESS_KEY'}
+    }
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,11 +79,10 @@ Rails.application.configure do
 
   # sets paperclip to upload images to amazon s3
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => 'S3_BUCKET_NAME', :access_key_id => 'AWS_ACCESS_KEY_ID', :secret_access_key => 'AWS_SECRET_ACCESS_KEY'}
     }
+  }
   }
 end
